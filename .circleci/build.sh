2a411e2fc4de888c45660184bdec9bdf851bc18d
fluenzi <74027909+fluenzi@users.noreply.github.com>
2020-12-28 16:32:33 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,1 +1,75 @@
-
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/https://github.com/fluenzi/android_kernel_realme_sdm710/ -b lineage18.1 kernel
+cd kernel
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3  AnyKernel
+git clone --depth=1 https://android.googlesource.com/platform/system/libufdt libufdt
+echo "Done"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+export CONFIG_PATH=$PWD/arch/arm64/configs/lineageos_RMX1851_defconfig
+PATH="${PWD}/clang/bin:$PATH"
+export ARCH=arm64
+export KBUILD_BUILD_HOST=circleci
+export KBUILD_BUILD_USER="fluenzi"
+# sticker plox
+function sticker() {
+    curl -s -X POST "https://api.telegram.org/bot1218234700:AAGfkmfWvuMZT-cqgkAeGc4jjTDsY-Fu9IQ/sendSticker" \
+        -d sticker="CAACAgEAAxkBAAEnKnJfZOFzBnwC3cPwiirjZdgTMBMLRAACugEAAkVfBy-aN927wS5blhsE" \
+        -d chat_id="1210616327"
+}
+# Send info plox channel
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot1218234700:AAGfkmfWvuMZT-cqgkAeGc4jjTDsY-Fu9IQ/sendMessage" \
+        -d chat_id="1210616327" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>• Predator-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7/7S</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
+}
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot1218234700:AAGfkmfWvuMZT-cqgkAeGc4jjTDsY-Fu9IQ/sendDocument" \
+        -F chat_id="1210616327" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7/7s (lavender)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot1218234700:AAGfkmfWvuMZT-cqgkAeGc4jjTDsY-Fu9IQ/sendMessage" \
+        -d chat_id="1210616327" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+# Compile plox
+function compile() {
+   make O=out ARCH=arm64 lineageos_RMX1851_defconfig
+       make -j$(nproc --all) O=out \
+                             ARCH=arm64 \
+			     CC=clang \
+			     CROSS_COMPILE=aarch64-linux-gnu- \
+			     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+   python2 "libufdt/utils/src/mkdtboimg.py" \
+					create "out/arch/arm64/boot/dtbo.img" --page_size=4096 out/arch/arm64/boot/dts/qcom/*.dtbo
+   cp out/arch/arm64/boot/dtbo.img AnyKernel
+}
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 test-${TANGGAL}.zip *
+    cd .. 
+}
+sticker
+sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
